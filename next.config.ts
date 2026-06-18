fa2871e9077e1a9f29fd5586d22807438ab4cacf
Gabin Kim <wlgiddl00001@gmail.com>
2026-06-18 16:40:34 +0900

    Add redirects for old seoul URLs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/seoul-:region/:service",
+        destination: "/:region/:service",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
